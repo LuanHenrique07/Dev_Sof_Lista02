57c607dac6e8b1510e6c72d622964bc80e5b70dc
Luan Almeida <luan.henrique.almeida@hotmail.com.br>
2024-03-18 23:15:09 -0300

    Oitavo Exercicio

---
+++ Exercicio08.java
@@ -0,0 +1,29 @@
+public class Exercicio08 {
+    public static void executar(){
+        int A[] = new int[6];
+        int B[] = new int[6];
+
+        for(int i = 1; i < A.length; i++){
+        A[i] = Prompt.lerInteiro("Digite o numero " + i + " do vetor A: ");
+        }
+        
+        for(int i = 1; i < B.length; i++){
+        B[i] = Prompt.lerInteiro("Digite o numero " + i + " do vetor B: ");
+        }
+
+        int[] C = new int[A.length + B.length];
+
+        for (int i = 0; i < A.length; i++) {
+            C[i] = A[i];
+        }
+
+        for (int i = 0; i < B.length; i++) {
+            C[A.length + i] = B[i];
+        }
+
+        Prompt.imprimir("Vetor C:");
+        for (int i = 0; i < C.length; i++) {
+            System.out.print(C[i] + " ");
+        }
+    }
+}
